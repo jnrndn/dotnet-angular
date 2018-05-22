26d73fa91ba12df604d715dbc784948de06e036c
Juan José Rendon <juan.rendon@yuxiglobal.com>
2018-05-21 22:56:17 -0500

    feat: value component + API call to get values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ValueComponent,
   ],
   imports: [
     BrowserModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/value/value.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.scss'],
+})
+export class ValueComponent implements OnInit {
+
+  values: number[];
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    this.http.get('http://localhost:5000/api/values')
+      .subscribe((response) => console.log(response));
+  }
+
+}
